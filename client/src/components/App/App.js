5e44b64b75f3b56269d2498fc91299d4863c8d36
Matheus <matheuscdasilva2@hotmail.com>
2019-08-07 13:06:19 -0300

    Adicionando styled-components e criando um CSS reset.

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { GlobalStyle } from './styles';
 
 class App extends Component {
     render() {
         return (
             <>
+                <GlobalStyle />
                 <h1>Hello World do React :)</h1>
             </>
         );
